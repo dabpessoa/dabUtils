ba6aee388f4516963064807b1ff5f0c90bb32319
dabpessoa <dabpessoa@dabpessoa-PC>
2016-10-25 21:21:04 -0300

    Correção ThreadUtils. Criação de pooling genérica.

--- src/me/dabpessoa/utils/ThreadUtils.java
+++ src/me/dabpessoa/utils/ThreadUtils.java
@@ -7,48 +7,90 @@ public class ThreadUtils {
 	
 	public static void main(String[] args) {
 		
-		ThreadUtils.createAndStartThreads(new LogicListener() {
+		ThreadUtils.doThreadPooling(new ThreadListener() {
 			
 			int valor = 10;
+			
 			@Override
-			public void doLogics(int threadNumber) {
+			public void proccess(ThreadBean threadBean, int threadNumber) {
 				System.out.println("imprimindo: "+threadNumber);
 				valor += 20;
 				System.out.println(valor);
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-		}, 5, 6);
+			
+			@Override
+			public void threadFinished(ThreadBean threadBean, int threadNumber) {
+				System.out.println("Thread Finished: "+threadNumber);
+			}
+			
+			@Override
+			public void allThreadsFinished(ThreadBean threadBean) {
+				System.out.println("FIM...");
+				
+			}
+		}, 2, 7);
 		
 	}
 	
-	private static List<Thread> threads;
-	static {
-		threads = new ArrayList<Thread>();
-	}
-	
-	public static void createAndStartThreads(LogicListener logicListener, int maxThreadsNumberAtSameTime, int totalThreadsToExecute) {
-		ThreadBean threadBean = new ThreadBean(logicListener, maxThreadsNumberAtSameTime, totalThreadsToExecute, 0);
+	public static void doThreadPooling(ThreadListener threadListener, int maxThreadsNumberAtSameTime, int totalThreadsToExecute) {
+		ThreadBean threadBean = new ThreadBean(threadListener, maxThreadsNumberAtSameTime, totalThreadsToExecute, 0);
 		proccess(threadBean);
 		
 	}
 	
-	public static void proccess(ThreadBean threadBean) {
+	private static void proccess(ThreadBean threadBean) {
 		
-		threads.clear();
-		while (threads.size() < threadBean.getMaxThreadsNumberAtSameTime() && threadBean.getThreadCount() < threadBean.getTotalThreadsToExecute()) {
+		threadBean.clearThreads();
+		if  (threadBean.getTotalThreadsToExecute() > threadBean.getThreadCount()) {
+			while (threadBean.threadsSize() < threadBean.getMaxThreadsNumberAtSameTime() && threadBean.getThreadCount() < threadBean.getTotalThreadsToExecute()) {
+				
+				MyRunnable runnable = new ThreadUtils().new MyRunnable(threadBean);
+				runnable.setLogicListener(threadBean.getLogicListener());
+				threadBean.addThread(new Thread(runnable));
+				
+			}
 			
-			MyRunnable runnable = new MyRunnable(threadBean, threads);
-			runnable.setLogicListener(threadBean.getLogicListener());
-			threads.add(new Thread(runnable));
-			
+			threadBean.startThreads();
 		}
 		
-		// start threads
-		for (Thread thread : threads) {
-			thread.start();
+		if (threadBean.isThreadsEmpty()) {
+			threadBean.getLogicListener().allThreadsFinished(threadBean);
 		}
 		
-		if (threads.isEmpty()) {
-			System.out.println("FIM...");
+	}
+	
+	class MyRunnable implements Runnable {
+		
+		private ThreadListener listener;
+		private ThreadBean threadBean;
+		private int threadNumber;
+		
+		public MyRunnable(ThreadBean threadBean) {
+			this.threadBean = threadBean;
+			this.threadBean.setThreadCount(this.threadBean.getThreadCount()+1);
+			this.threadNumber = this.threadBean.getThreadCount();
+		}
+		
+		@Override
+		public void run() {
+			listener.proccess(threadBean, threadNumber);
+			endVerify();
+		}
+		
+		public void endVerify() {
+			threadBean.getLogicListener().threadFinished(threadBean, threadNumber);
+			if (threadBean.isOnlyOneThreadAlive()) {
+				proccess(threadBean);
+			}
+		}
+		
+		public void setLogicListener(ThreadListener listener) {
+			this.listener = listener;
 		}
 		
 	}
@@ -56,70 +98,32 @@ public class ThreadUtils {
 }
 
 class ThreadBean {
-	private LogicListener logicListener;
+	private List<Thread> threads;
+	private ThreadListener logicListener;
 	private int maxThreadsNumberAtSameTime;
 	private int totalThreadsToExecute;
 	private int threadCount;
-	public ThreadBean() {}
-	public ThreadBean(LogicListener logicListener, int maxThreadsNumberAtSameTime, int totalThreadsToExecute, int threadCount) {
+	protected ThreadBean() {}
+	protected ThreadBean(ThreadListener logicListener, int maxThreadsNumberAtSameTime, int totalThreadsToExecute, int threadCount) {
+		this.threads = new ArrayList<Thread>();
 		this.logicListener = logicListener;
 		this.maxThreadsNumberAtSameTime = maxThreadsNumberAtSameTime;
 		this.totalThreadsToExecute = totalThreadsToExecute;
 		this.threadCount = threadCount;
 	}
-	public LogicListener getLogicListener() {
-		return logicListener;
+	protected void addThread(Thread thread) {
+		threads.add(thread);
 	}
-	public void setLogicListener(LogicListener logicListener) {
-		this.logicListener = logicListener;
+	protected void clearThreads() {
+		threads.clear();
 	}
-	public int getMaxThreadsNumberAtSameTime() {
-		return maxThreadsNumberAtSameTime;
+	protected int threadsSize() {
+		return threads.size();
 	}
-	public void setMaxThreadsNumberAtSameTime(int maxThreadsNumberAtSameTime) {
-		this.maxThreadsNumberAtSameTime = maxThreadsNumberAtSameTime;
+	protected boolean isThreadsEmpty() {
+		return threads.isEmpty();
 	}
-	public int getTotalThreadsToExecute() {
-		return totalThreadsToExecute;
-	}
-	public void setTotalThreadsToExecute(int totalThreadsToExecute) {
-		this.totalThreadsToExecute = totalThreadsToExecute;
-	}
-	public int getThreadCount() {
-		return threadCount;
-	}
-	public void setThreadCount(int threadCount) {
-		this.threadCount = threadCount;
-	}
-}
-
-class MyRunnable implements Runnable {
-	
-	private LogicListener listener;
-	private List<Thread> threads;
-	private ThreadBean threadBean;
-	
-	public MyRunnable(ThreadBean threadBean, List<Thread> threads) {
-		this.threadBean = threadBean;
-		this.threadBean.setThreadCount(this.threadBean.getThreadCount()+1);
-		this.threads = threads;
-	}
-	
-	@Override
-	public void run() {
-		listener.doLogics(threadBean.getThreadCount());
-		endVerify();
-	}
-	
-	public void endVerify() {
-		if (isLastThreadFinishing()) {
-			if  (threadBean.getTotalThreadsToExecute() > threadBean.getThreadCount()) {
-				ThreadUtils.proccess(threadBean);
-			}
-		}
-	}
-	
-	public synchronized boolean isLastThreadFinishing() {
+	protected synchronized boolean isOnlyOneThreadAlive() {
 		int aliveCount = 0;
 		for (Thread thread : threads) {
 			if (thread.isAlive()) aliveCount++;			
@@ -128,17 +132,48 @@ class MyRunnable implements Runnable {
 		if (aliveCount == 1) return true;
 		else return false;
 	}
-	
-	public void setLogicListener(LogicListener listener) {
-		this.listener = listener;
+	protected synchronized void startThreads() {
+		if (threads != null) {
+			for (Thread thread : threads) {
+				thread.start();
+			}
+		}
 	}
-	
+	public ThreadListener getLogicListener() {
+		return logicListener;
+	}
+	protected void setLogicListener(ThreadListener logicListener) {
+		this.logicListener = logicListener;
+	}
+	public int getMaxThreadsNumberAtSameTime() {
+		return maxThreadsNumberAtSameTime;
+	}
+	protected void setMaxThreadsNumberAtSameTime(int maxThreadsNumberAtSameTime) {
+		this.maxThreadsNumberAtSameTime = maxThreadsNumberAtSameTime;
+	}
+	public int getTotalThreadsToExecute() {
+		return totalThreadsToExecute;
+	}
+	protected void setTotalThreadsToExecute(int totalThreadsToExecute) {
+		this.totalThreadsToExecute = totalThreadsToExecute;
+	}
+	public int getThreadCount() {
+		return threadCount;
+	}
+	protected void setThreadCount(int threadCount) {
+		this.threadCount = threadCount;
+	}
+	protected void setThreads(List<Thread> threads) {
+		this.threads = threads;
+	}
 }
 
-interface LogicListener {
-	void doLogics(int threadNumber);
+interface ThreadListener {
+	void proccess(ThreadBean threadBean, int threadNumber);
+	void threadFinished(ThreadBean threadBean, int threadNumber);
+	void allThreadsFinished(ThreadBean threadBean);
 }
 
 interface EndListener {
-	void endNotify();
+	void endNotify(ThreadBean threadBean);
 }
